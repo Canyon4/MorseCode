b34682c12b6d23c7e4b59e1ea14af192d6dee2a8
Canyon Sorrel <143111829+Canyon4@users.noreply.github.com>
2023-11-09 10:43:59 -0700

    Update morse.py

--- morse.py
+++ morse.py
@@ -1,18 +1,19 @@
+#imports GPIO, time, and the translator
 import RPi.GPIO as GPIO
 import time
 from pyMorseTranslator import translator
-
+# creates encoder, gets input from user, encodes input
 encoder = translator.Encoder()
 inp = input("give input: ")
 a = encoder.encode(inp).morse
-
+# sets up GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(19,GPIO.OUT)
 p = GPIO.PWM(19,50)
-
+# makes string "a" into a list
 a.split(" ")
-
+# runs through "a" and checks symbol, and sends different signal to buzzer depending on the symbol.
 for i in range(len(a)):
 
 	if (a[i-1] == "."):
